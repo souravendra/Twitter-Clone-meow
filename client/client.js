c4bfb52c66d02ba5240219cfc634f525d29a14b7
souravendra <51351911+souravendra@users.noreply.github.com>
2020-02-24 03:17:36 +0530

    DB

--- client/client.js
+++ client/client.js
@@ -1,16 +1,57 @@
 console.log("Hello World");
 
-const form =  document.querySelector('form');
+const form =  document.querySelector('form'); //grabs an element on the page
+const loadingElement = document.querySelector('.loading');
+const mewsElement = document.querySelector('.mews');
+const API_URL = 'http://localhost:5000/mews';
+
+loadingElement.style.display = '';
+
+listAllMews();
 
 form.addEventListener('submit', (event)=> {
     event.preventDefault(); //prevents auto-reload
     const formData = new FormData(form);
     const name = formData.get('name');
     const content = formData.get('content');
-
+    
+    //grabbing form data and putting it into an object then sending it to the back-end
     const mew = {
         name,
         content
     };  
-    console.log(mew);
+
+    form.style.display = 'none';
+    loadingElement.style.display = '';
+
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(mew),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })  .then(response => response.json())
+        .then(createdMew => {
+            console.log(createdMew);
+            form.reset();
+            form.style.display = '';
+            loadingElement.style.display = 'none';
+        });
 });
+
+function listAllMews(){
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(mews => {
+            console.log(mews);
+            mews.forEach(mew => {
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = mew.name;
+
+                const contents = document.createElement('p');
+                contents.textContent = mew.content;
+            });
+        });
+}
