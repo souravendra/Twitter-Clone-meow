3d607475a9840d413f4acff96513f6f7d17d1894
souravendra <51351911+souravendra@users.noreply.github.com>
2020-02-24 00:46:42 +0530

    FormData

--- client/client.js
+++ client/client.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+const form =  document.querySelector('form');
+
+form.addEventListener('submit', (event)=> {
+    event.preventDefault(); //prevents auto-reload
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const mew = {
+        name,
+        content
+    };  
+    console.log(mew);
+});
